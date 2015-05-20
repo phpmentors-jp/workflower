bc53b5e5c3b6773b22c9ca4085758a551b7409c1
KUBO Atsuhiro <kubo@iteman.jp>
2015-05-21 08:00:51 +0900

    add `WorkflowRepositoryInterface`

---
+++ src/Workflow/WorkflowRepositoryInterface.php
@@ -0,0 +1,24 @@
+<?php
+/*
+ * Copyright (c) 2015 KUBO Atsuhiro <kubo@iteman.jp>,
+ * All rights reserved.
+ *
+ * This file is part of Workflower.
+ *
+ * This program and the accompanying materials are made available under
+ * the terms of the BSD 2-Clause License which accompanies this
+ * distribution, and is available at http://opensource.org/licenses/BSD-2-Clause
+ */
+
+namespace PHPMentors\Workflower\Workflow;
+
+use PHPMentors\DomainKata\Repository\RepositoryInterface;
+
+interface WorkflowRepositoryInterface extends RepositoryInterface
+{
+    /**
+     * @param int|string $id
+     * @return Workflow
+     */
+    public function findById($id);
+}
