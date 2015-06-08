96990bc6bad4b8648f33539456358b5c0e149d1c
KUBO Atsuhiro <kubo@iteman.jp>
2015-06-08 14:46:07 +0900

    add `Bpmn2File`

---
+++ src/Definition/Bpmn2File.php
@@ -0,0 +1,58 @@
+<?php
+/*
+ * Copyright (c) 2015 KUBO Atsuhiro <kubo@iteman.jp>,
+ * All rights reserved.
+ *
+ * This file is part of Workflower.
+ *
+ * This program and the accompanying materials are made available under
+ * the terms of the BSD 2-Clause License which accompanies this
+ * distribution, and is available at http://opensource.org/licenses/BSD-2-Clause
+ */
+
+namespace PHPMentors\Workflower\Definition;
+
+use PHPMentors\DomainKata\Entity\EntityInterface;
+use PHPMentors\DomainKata\Entity\Operation\IdentifiableInterface;
+
+class Bpmn2File implements EntityInterface, IdentifiableInterface
+{
+    /**
+     * @var string
+     */
+    private $file;
+
+    /**
+     * @param string $file
+     */
+    public function __construct($file)
+    {
+        $this->file = $file;
+    }
+
+    /**
+     * @return string
+     */
+    public function getId()
+    {
+        return static::getWorkflowId($this->file);
+    }
+
+    /**
+     * @return string
+     */
+    public function getFile()
+    {
+        return $this->file;
+    }
+
+    /**
+     * @param string $file
+     *
+     * @return string
+     */
+    public static function getWorkflowId($file)
+    {
+        return pathinfo($file, PATHINFO_FILENAME);
+    }
+}
