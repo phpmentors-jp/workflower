6565d9e30a50545d238ce7c7f826584f59cacad4
KUBO Atsuhiro <kubo@iteman.jp>
2015-06-17 14:51:27 +0900

    add `ResourceInterface`

---
+++ src/Workflow/Resource/ResourceInterface.php
@@ -0,0 +1,23 @@
+<?php
+/*
+ * Copyright (c) 2015 KUBO Atsuhiro <kubo@iteman.jp>,
+ * All rights reserved.
+ *
+ * This file is part of Workflower.
+ *
+ * This program and the accompanying materials are made available under
+ * the terms of the BSD 2-Clause License which accompanies this
+ * distribution, and is available at http://opensource.org/licenses/BSD-2-Clause
+ */
+
+namespace PHPMentors\Workflower\Workflow\Resource;
+
+use PHPMentors\DomainKata\Entity\Operation\IdentifiableInterface;
+
+interface ResourceInterface extends OperationInterface, IdentifiableInterface
+{
+    /**
+     * @return string
+     */
+    public function getName();
+}
