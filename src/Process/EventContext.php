b7eb4300744218d884ef73c4dfb64cc640b3d435
KUBO Atsuhiro <kubo@iteman.jp>
2015-06-18 02:32:13 +0900

    add `EventContext`

---
+++ src/Process/EventContext.php
@@ -0,0 +1,52 @@
+<?php
+/*
+ * Copyright (c) 2015 KUBO Atsuhiro <kubo@iteman.jp>,
+ * All rights reserved.
+ *
+ * This file is part of Workflower.
+ *
+ * This program and the accompanying materials are made available under
+ * the terms of the BSD 2-Clause License which accompanies this
+ * distribution, and is available at http://opensource.org/licenses/BSD-2-Clause
+ */
+
+namespace PHPMentors\Workflower\Process;
+
+class EventContext implements EventContextInterface
+{
+    /**
+     * @var int|string
+     */
+    private $eventId;
+
+    /**
+     * @var ProcessContextInterface
+     */
+    private $processContext;
+
+    /**
+     * @param int|string              $eventId
+     * @param ProcessContextInterface $processContext
+     */
+    public function __construct($eventId, ProcessContextInterface $processContext)
+    {
+        $this->eventId = $eventId;
+        $this->processContext = $processContext;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getEventId()
+    {
+        return $this->eventId;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getProcessContext()
+    {
+        return $this->processContext;
+    }
+}
