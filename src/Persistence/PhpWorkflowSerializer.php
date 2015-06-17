118fb24c5ff6455b6f769910fa103b1c8f770be5
KUBO Atsuhiro <kubo@iteman.jp>
2015-06-18 04:14:41 +0900

    add `PhpWorkflowSerializer`

---
+++ src/Persistence/PhpWorkflowSerializer.php
@@ -0,0 +1,34 @@
+<?php
+/*
+ * Copyright (c) 2015 KUBO Atsuhiro <kubo@iteman.jp>,
+ * All rights reserved.
+ *
+ * This file is part of Workflower.
+ *
+ * This program and the accompanying materials are made available under
+ * the terms of the BSD 2-Clause License which accompanies this
+ * distribution, and is available at http://opensource.org/licenses/BSD-2-Clause
+ */
+
+namespace PHPMentors\Workflower\Persistence;
+
+use PHPMentors\Workflower\Workflow\Workflow;
+
+class PhpWorkflowSerializer implements WorkflowSerializerInterface
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function serialize(Workflow $workflow)
+    {
+        return serialize($workflow);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function deserialize($workflow)
+    {
+        return unserialize($workflow);
+    }
+}
