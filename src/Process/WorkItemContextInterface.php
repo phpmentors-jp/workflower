d6669c258a8fb7de865a2b6d3d0b838d6ac23c82
KUBO Atsuhiro <kubo@iteman.jp>
2015-06-17 15:52:14 +0900

    add `WorkItemContextInterface`

---
+++ src/Process/WorkItemContextInterface.php
@@ -0,0 +1,34 @@
+<?php
+/*
+ * Copyright (c) 2015 KUBO Atsuhiro <kubo@iteman.jp>,
+ * All rights reserved.
+ *
+ * This file is part of Workflower.
+ *
+ * This program and the accompanying materials are made available under
+ * the terms of the BSD 2-Clause License which accompanies this
+ * distribution, and is available at http://opensource.org/licenses/BSD-2-Clause
+ */
+
+namespace PHPMentors\Workflower\Process;
+
+use PHPMentors\DomainKata\Entity\EntityInterface;
+use PHPMentors\Workflower\Workflow\Participant\ParticipantInterface;
+
+interface WorkItemContextInterface extends EntityInterface
+{
+    /**
+     * @return int|string
+     */
+    public function getActivityId();
+
+    /**
+     * @return ParticipantInterface
+     */
+    public function getParticipant();
+
+    /**
+     * @return ProcessContextInterface
+     */
+    public function getProcessContext();
+}
