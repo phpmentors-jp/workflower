9aac0d8ba47c69596204aeda5771af59f2e61576
KUBO Atsuhiro <kubo@iteman.jp>
2015-06-18 04:14:26 +0900

    add `WorkflowSerializerInterface`

---
+++ src/Persistence/WorkflowSerializerInterface.php
@@ -0,0 +1,33 @@
+<?php
+/*
+ * Copyright (c) 2015 KUBO Atsuhiro <kubo@iteman.jp>,
+ * All rights reserved.
+ *
+ * This file is part of Workflower.
+ *
+ * This program and the accompanying materials are made available under
+ * the terms of the BSD 2-Clause License which accompanies this
+ * distribution, and is available at http://opensource.org/licenses/BSD-2-Clause
+ */
+
+namespace PHPMentors\Workflower\Persistence;
+
+use PHPMentors\DomainKata\Service\ServiceInterface;
+use PHPMentors\Workflower\Workflow\Workflow;
+
+interface WorkflowSerializerInterface extends ServiceInterface
+{
+    /**
+     * @param Workflow $workflow
+     *
+     * @return string
+     */
+    public function serialize(Workflow $workflow);
+
+    /**
+     * @param string $workflow
+     *
+     * @return Workflow
+     */
+    public function deserialize($workflow);
+}
