2584379525a90c3e71390e3ea29505f6aa759585
Timothy Clissold <site@timothyclissold.com>
2018-06-01 16:02:23 +1000

    Added ParallelGateway

---
+++ src/Workflow/Gateway/ParallelGateway.php
@@ -0,0 +1,130 @@
+<?php
+/*
+ * Copyright (c) KUBO Atsuhiro <kubo@iteman.jp> and contributors,
+ * All rights reserved.
+ *
+ * This file is part of Workflower.
+ *
+ * This program and the accompanying materials are made available under
+ * the terms of the BSD 2-Clause License which accompanies this
+ * distribution, and is available at http://opensource.org/licenses/BSD-2-Clause
+ */
+
+namespace PHPMentors\Workflower\Workflow\Gateway;
+
+use PHPMentors\DomainKata\Entity\EntityInterface;
+use PHPMentors\Workflower\Workflow\Participant\Role;
+
+class ParallelGateway implements GatewayInterface, \Serializable
+{
+    /**
+     * @var int|string
+     */
+    private $id;
+
+    /**
+     * @var string
+     */
+    private $name;
+
+    /**
+     * @var Role
+     */
+    private $role;
+
+    /**
+     * @var int|string
+     */
+    private $defaultSequenceFlowId;
+
+    /**
+     * @param int|string $id
+     * @param Role       $role
+     * @param string     $name
+     */
+    public function __construct($id, Role $role, $name = null)
+    {
+        $this->id = $id;
+        $this->role = $role;
+        $this->name = $name;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function serialize()
+    {
+        return serialize(array(
+            'id' => $this->id,
+            'name' => $this->name,
+            'role' => $this->role,
+            'defaultSequenceFlow' => $this->defaultSequenceFlowId,
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function unserialize($serialized)
+    {
+        foreach (unserialize($serialized) as $name => $value) {
+            if (property_exists($this, $name)) {
+                $this->$name = $value;
+            }
+        }
+    }
+
+    /**
+     * {@inheritdoc}
+     *
+     * @return int|string
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getRole()
+    {
+        return $this->role;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function equals(EntityInterface $target)
+    {
+        if (!($target instanceof self)) {
+            return false;
+        }
+
+        return $this->id === $target->getId();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setDefaultSequenceFlowId($sequenceFlowId)
+    {
+        $this->defaultSequenceFlowId = $sequenceFlowId;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getDefaultSequenceFlowId()
+    {
+        return $this->defaultSequenceFlowId;
+    }
+}
