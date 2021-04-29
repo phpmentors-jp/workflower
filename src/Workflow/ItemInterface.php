023cbb5af6871839326ea12f0cc7a8a5b5de07a6
Atsuhiro Kubo <kubo@iteman.jp>
2021-04-29 16:30:16 +0900

    add missing comments

--- src/Workflow/ItemInterface.php
+++ src/Workflow/ItemInterface.php
@@ -1,9 +1,22 @@
 <?php
+/*
+ * Copyright (c) Atsuhiro Kubo <kubo@iteman.jp> and contributors,
+ * All rights reserved.
+ *
+ * This file is part of Workflower.
+ *
+ * This program and the accompanying materials are made available under
+ * the terms of the BSD 2-Clause License which accompanies this
+ * distribution, and is available at http://opensource.org/licenses/BSD-2-Clause
+ */
 
 namespace PHPMentors\Workflower\Workflow;
 
 use PHPMentors\Workflower\Workflow\Activity\ActivityInterface;
 
+/**
+ * @since Interface available since Release 2.0.0
+ */
 interface ItemInterface
 {
     /**
