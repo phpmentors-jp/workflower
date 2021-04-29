f95e21917964ecc0ce8559cf30033073c58376f5
Atsuhiro Kubo <kubo@iteman.jp>
2021-04-29 16:16:44 +0900

    add missing comments

--- src/Workflow/Activity/OperationalTask.php
+++ src/Workflow/Activity/OperationalTask.php
@@ -1,9 +1,22 @@
 <?php
+/*
+ * Copyright (c) Atsuhiro Kubo <kubo@iteman.jp> and contributors,
+ * All rights reserved.
+ *
+ * This file is part of Workflower.
+ *
+ * This program and the accompanying materials are made available under
+ * the terms of the BSD 2-Clause License which accompanies this
+ * distribution, and is available at http://opensource.org/licenses/BSD-2-Clause
+ */
 
 namespace PHPMentors\Workflower\Workflow\Activity;
 
 use PHPMentors\Workflower\Workflow\Operation\OperationalInterface;
 
+/**
+ * @since Class available since Release 2.0.0
+ */
 abstract class OperationalTask extends Task implements OperationalInterface
 {
     /**
@@ -81,7 +94,7 @@ abstract class OperationalTask extends Task implements OperationalInterface
             if (!$workItem->isEnded()) {
                 $workItem->allocate($participant);
                 $workItem->start();
-                $operationRunner->runWorkItem($workItem);
+                $operationRunner->run($workItem);
                 $workItem->complete($participant);
             }
         }
